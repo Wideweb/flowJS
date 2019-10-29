bceb1c47da4c3411f6b1a3568950bde4873bd7e1
Alckevich <alckevich@live.com>
2019-10-30 00:20:22 +0300

    a-start

--- src/map/game-map.ts
+++ src/map/game-map.ts
@@ -1,42 +1,73 @@
 import MapCell, { MapCellType } from './map-cell';
 import { Container } from 'pixi.js';
+import AStar, { IGraph, GraphConnection, IHeuristic } from '../pathfinding/a-star';
 
-export default class GameMap {
+class Heuristic implements IHeuristic<MapCell> {
+	estimate(from: MapCell, to: MapCell): number {
+		return (Math.abs(from.x - to.x) + Math.abs(from.y - to.y)) * 10;
+	}
+}
 
-    public cells: MapCell[][];
+export default class GameMap implements IGraph<MapCell> {
 
-    constructor(
-        private data: number[][],
-        private width: number,
-        private height: number,
-    ) { }
+	public cells: MapCell[][];
 
-    load(parent: Container): void {
-        const cellWidth = this.width / this.data[0].length;
-        const cellHeight = this.height / this.data.length;
-        this.cells = [];
-        for (let y = 0; y < this.data.length; y++) {
-            this.cells.push([]);
-            for (let x = 0; x < this.data[0].length; x++) {
-                const cell = new MapCell(
-                    x * cellWidth,
-                    y * cellHeight,
-                    this.data[y][x] as MapCellType,
-                    cellWidth,
-                    cellHeight,
-                );
-                cell.load(parent);
-                this.cells[y].push(cell);
-            }
-        }
-    }
+	private dx: number[] = [0, 1, -1];
+	private dy: number[] = [0, 1, -1];
+	private pathFinder = new AStar<MapCell>();
 
-    unload(parent: Container): void {
-        this.cells.forEach(row => row.forEach(cell => cell.unload(parent)));
-        this.cells.length = 0;
-    }
 
-    update(): void {
-        this.cells.forEach(row => row.forEach(cell => cell.update()));
-    }
+	constructor(
+		private data: number[][],
+		private width: number,
+		private height: number,
+	) { }
+
+	getPath(from: MapCell, to: MapCell) {
+		return this.pathFinder.getPath(this, from, to, new Heuristic());
+	}
+
+	getConnections(node: MapCell): GraphConnection<MapCell>[] {
+		const connections: GraphConnection<MapCell>[] = [];
+		for (let i = 0; i < this.dy.length; i++) {
+			for (let j = 0; j < this.dx.length; j++) {
+				const x = node.x + this.dx[i];
+				const y = node.y + this.dy[i];
+				const cost = this.dx[i] !== 0 && this.dy[i] !== 0 ? 14 : 10;
+				if (x >= 0 && y >= 0 && x < this.data[0].length && y < this.data.length) {
+					connections.push(new GraphConnection(node, this.cells[y][x], cost));
+				}
+			}
+		}
+		return connections;
+	}
+
+	load(parent: Container): void {
+		const cellWidth = this.width / this.data[0].length;
+		const cellHeight = this.height / this.data.length;
+		this.cells = [];
+		for (let y = 0; y < this.data.length; y++) {
+			this.cells.push([]);
+			for (let x = 0; x < this.data[0].length; x++) {
+				const cell = new MapCell(
+					x * cellWidth,
+					y * cellHeight,
+					this.data[y][x] as MapCellType,
+					cellWidth,
+					cellHeight,
+				);
+				cell.load(parent);
+				this.cells[y].push(cell);
+			}
+		}
+	}
+
+	unload(parent: Container): void {
+		this.cells.forEach(row => row.forEach(cell => cell.unload(parent)));
+		this.cells.length = 0;
+	}
+
+	update(): void {
+		this.cells.forEach(row => row.forEach(cell => cell.update()));
+	}
 }
--- src/pathfinding/a-star.ts
+++ src/pathfinding/a-star.ts
@@ -1,167 +1,160 @@
-class GraphNode {
+export class GraphConnection<T> {
+
+	constructor(
+		public from: T,
+		public to: T,
+		public cost: number,
+	) { }
 }
 
-class GraphConnection {
-    getToNode(): GraphNode {
-        return new GraphNode();
-    }
-
-    getCost(): number {
-        return 1;
-    }
+export interface IGraph<T> {
+	getConnections(node: T): GraphConnection<T>[];
 }
 
-class Graph {
-    getConnections(node: GraphNode): GraphConnection[] {
-        return [];
-    }
+class NodeRecord<T> implements IComparable {
+	public node: T;
+	public connection: any;
+	public estimatedTotalCost: number;
+	public cost: number = Infinity;
+
+	public compareTo(node: NodeRecord<T>): number {
+		return this.estimatedTotalCost - node.estimatedTotalCost;
+	}
 }
 
-
-class NodeRecord implements IComparable {
-    public node: GraphNode;
-    public connection: any;
-    public estimatedTotalCost: number;
-    public cost: number = Infinity;
-
-    public compareTo(node: NodeRecord): number {
-        return this.estimatedTotalCost - node.estimatedTotalCost;
-    }
-}
-
-interface IHeuristic {
-    estimate(from: GraphNode, to: GraphNode): number;
+export interface IHeuristic<T> {
+	estimate(from: T, to: T): number;
 }
 
 interface IComparable {
-    compareTo(v: IComparable): number;
+	compareTo(v: IComparable): number;
 }
 
 class PriorityQueue<T extends IComparable> {
-    private list: T[] = [];
-    
-    contains(predicate: (node: T) => boolean): boolean {
-        return this.list.some(predicate);
-    }
+	private list: T[] = [];
 
-    find(predicate: (node: T) => boolean) {
-        return this.list.find(predicate);
-    }
+	contains(predicate: (node: T) => boolean): boolean {
+		return this.list.some(predicate);
+	}
 
-    length(): number {
-        return this.list.length;
-    }
+	find(predicate: (node: T) => boolean) {
+		return this.list.find(predicate);
+	}
 
-    public enqueue(element: T) {
-        this.list.push(element);
-        this.up(this.list.length - 1);
-    }
+	length(): number {
+		return this.list.length;
+	}
 
-    public dequeue() {
-        const element = this.list[0];
-        this.swap(0, --this.list.length);
-        this.down(0);
-        return element;
-    }
+	public enqueue(element: T) {
+		this.list.push(element);
+		this.up(this.list.length - 1);
+	}
 
-    private up(i: number): void {
-        //Check whether the current element bigger than child
-        while (i != 0 && this.list[i].compareTo(this.list[(i - 1) / 2]) > 0) {
-            this.swap(i, (i - 1) / 2);
-            i = (i - 1) / 2;
-        }
-    }
+	public dequeue() {
+		const element = this.list[0];
+		this.swap(0, --this.list.length);
+		this.down(0);
+		return element;
+	}
 
-    private down(i: number): void {
-        while (i < this.list.length / 2) {
-            let maxI = 2 * i + 1;
+	private up(i: number): void {
+		//Check whether the current element bigger than child
+		while (i != 0 && this.list[i].compareTo(this.list[(i - 1) / 2]) > 0) {
+			this.swap(i, (i - 1) / 2);
+			i = (i - 1) / 2;
+		}
+	}
 
-            //Take the biggest child
-            if (2 * i + 2 < this.list.length && this.list[2 * i + 2].compareTo(this.list[2 * i + 1]) > 0) {
-                maxI = 2 * i + 2;
-            }
+	private down(i: number): void {
+		while (i < this.list.length / 2) {
+			let maxI = 2 * i + 1;
 
-            //Check whether the current element bigger than child
-            if (this.list[i].compareTo(this.list[maxI]) >= 0) {
-                return;
-            }
+			//Take the biggest child
+			if (2 * i + 2 < this.list.length && this.list[2 * i + 2].compareTo(this.list[2 * i + 1]) > 0) {
+				maxI = 2 * i + 2;
+			}
 
-            this.swap(i, maxI);
-            i = maxI;
-        }
-    }
+			//Check whether the current element bigger than child
+			if (this.list[i].compareTo(this.list[maxI]) >= 0) {
+				return;
+			}
 
-    private swap(fistIndex: number, secondIndex: number) {
-        const firstElement = this.list[fistIndex];
-        this.list[fistIndex] = this.list[secondIndex];
-        this.list[secondIndex] = firstElement;
-    }
+			this.swap(i, maxI);
+			i = maxI;
+		}
+	}
+
+	private swap(fistIndex: number, secondIndex: number) {
+		const firstElement = this.list[fistIndex];
+		this.list[fistIndex] = this.list[secondIndex];
+		this.list[secondIndex] = firstElement;
+	}
 }
 
-export default class AStar {
+export default class AStar<T> {
 
-    getPath(graph: Graph, start: GraphNode, end: GraphNode, heuristic: IHeuristic) {
-        const startRecord = new NodeRecord();
-        startRecord.node = start;
-        startRecord.connection = null;
-        startRecord.cost = 0;
-        startRecord.estimatedTotalCost = heuristic.estimate(start, end);
+	getPath(graph: IGraph<T>, start: T, end: T, heuristic: IHeuristic<T>) {
+		const startRecord = new NodeRecord<T>();
+		startRecord.node = start;
+		startRecord.connection = null;
+		startRecord.cost = 0;
+		startRecord.estimatedTotalCost = heuristic.estimate(start, end);
 
-        const open = new PriorityQueue<NodeRecord>();
-        open.enqueue(startRecord);
-        const closed: NodeRecord[] = [];
+		const open = new PriorityQueue<NodeRecord<T>>();
+		open.enqueue(startRecord);
+		const closed: NodeRecord<T>[] = [];
 
-        let current: NodeRecord;
+		let current: NodeRecord<T>;
 
-        while (open.length() > 0) {
-            current = open.dequeue();
-            if (current.node == end) {
-                break;
-            }
+		while (open.length() > 0) {
+			current = open.dequeue();
+			if (current.node == end) {
+				break;
+			}
 
-            const connections = graph.getConnections(current)
-            for (let connection of connections) {
-                const endNode = connection.getToNode();
-                const endNodeCost = current.cost + connection.getCost();
+			const connections = graph.getConnections(current.node)
+			for (let connection of connections) {
+				const endNode = connection.to;
+				const endNodeCost = current.cost + connection.cost;
 
-                if (closed.some(record => record.node === endNode)) {
-                    continue;
-                }
+				if (closed.some(record => record.node === endNode)) {
+					continue;
+				}
 
-                let endNodeRecord = open.find(record => record.node === endNode);
-                const isInOpen = !!endNodeRecord;
+				let endNodeRecord = open.find(record => record.node === endNode);
+				const isInOpen = !!endNodeRecord;
 
-                if (!endNodeRecord) {
-                    endNodeRecord = new NodeRecord();
-                    endNodeRecord.node = endNode;
-                } else if (endNodeRecord.cost <= endNodeCost) {
-                    continue;
-                }
+				if (!endNodeRecord) {
+					endNodeRecord = new NodeRecord();
+					endNodeRecord.node = endNode;
+				} else if (endNodeRecord.cost <= endNodeCost) {
+					continue;
+				}
 
-                endNodeRecord.cost = endNodeCost;
-                endNodeRecord.connection = connection;
-                endNodeRecord.estimatedTotalCost = endNodeCost + heuristic.estimate(start, endNodeRecord.node);
+				endNodeRecord.cost = endNodeCost;
+				endNodeRecord.connection = connection;
+				endNodeRecord.estimatedTotalCost = endNodeCost + heuristic.estimate(start, endNodeRecord.node);
 
-                if (!isInOpen) {
-                    open.enqueue(endNodeRecord);
-                }
-            }
+				if (!isInOpen) {
+					open.enqueue(endNodeRecord);
+				}
+			}
 
-            open.dequeue();
-            closed.splice(closed.indexOf(current), 1);
-        }
+			open.dequeue();
+			closed.splice(closed.indexOf(current), 1);
+		}
 
-        if (current.node != end) {
-            return [];
-        }
+		if (current.node != end) {
+			return [];
+		}
 
-        const path: GraphConnection[] = [];
+		const path: GraphConnection<T>[] = [];
 
-        while (current.node != start) {
-            path.push(current.connection);
-            current = current.connection.getFromNode();
-        }
+		while (current.node != start) {
+			path.push(current.connection);
+			current = current.connection.getFromNode();
+		}
 
-        return path.reverse();
-    }
+		return path.reverse();
+	}
 }
